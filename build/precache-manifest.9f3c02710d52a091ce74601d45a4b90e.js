f919dbf1a3c0e4002ed555f83f66fc3859a27d20
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-04-16 11:04:12 +1000

    deleted old .gitignore and added a new one

---
+++ build/precache-manifest.9f3c02710d52a091ce74601d45a4b90e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609f32d14cea5fd3b52fe13065de7ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1401fad28a2a3376b4cb",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c103181fe871aafb8d36",
+    "url": "/static/js/2.16dba76a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16dba76a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1401fad28a2a3376b4cb",
+    "url": "/static/js/main.be3a3a72.chunk.js"
+  },
+  {
+    "revision": "4511b96ea7c033413b01",
+    "url": "/static/js/runtime-main.86512954.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
