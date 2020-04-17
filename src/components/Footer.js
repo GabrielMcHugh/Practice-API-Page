ff8438f1fa3c430c3baa61fa7b4c87d4164cb478
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-04-17 10:59:10 +1000

    added Header and Footer component + removed the default App contents so that returns the new components that I created

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div style={{backgroundColor: '#ffe14e', height: 'calc(100% - 60px)'}}>
+			Footer Card
+		</div>
+	);
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div style={{backgroundColor: 'lightblue'}}>
+			Title Card
+		</div>
+	);
+}
+
+export default Header;
