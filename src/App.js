136325a6edef2840daf05de6e314c5805fd04c54
Gabriel McHugh <gabriel.t.mchugh@gmail.com>
2020-04-17 11:52:06 +1000

    removed react-flexview because I was unnecessarily complicating my task.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import FlexView from 'react-flexview';
 import './App.css';
 
 function App() {
+  //Problem 1: pushing the footer to the bottom of the page
+  // - installed FlexView, a type of Flexbox package for react
+  // - Made a HeaderContainer class in css file. Isn't working
+  // - [HERE] attempted to implement a flexbox around the components but was unsucessful
+  // + keep reading the FlexView page to try and understand it so that you can implement it
+  // + IF you can't implement it, consider using another flexbox package
   return (
-    <FlexView column hAlignContent='center'>
-      <div className = 'HeaderContainer' ><Header/></div>
-      <FlexView><Footer/></FlexView>
-    </FlexView>
+    <div column>
+      <Header/>
+      <Footer/>
+    </div>
   );
 }
 
